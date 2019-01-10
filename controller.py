02a8261c323d6915595e814a6f8df3a6daf6542d
OnlyBrezel <onlybrezel@gmail.com>
2019-01-10 18:16:22 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -1015,15 +1015,19 @@ def handle_command(args):
                 if command == 'SI':
                     incrementMSpeed(10)
                     time.sleep(0.05)
+                    return
                 if command == 'SD':
                     incrementMSpeed(-10)
                     time.sleep(0.05)
+                    return
                 if command == 'TI':
                     incrementTSpeed(10)
                     time.sleep(0.05)
+                    return
                 if command == 'TD':
                     incrementTSpeed(-10)
                     time.sleep(0.05)
+                    return
                 handle_mebo_command(command)
             
             if commandArgs.type == 'gopigo2':
